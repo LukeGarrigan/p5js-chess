be4365da001a39db2fe9c8de2ec49743aadee208
Luke <Lukegarrigan8@gmail.com>
2020-11-23 20:48:06 +0000

    Can now promote to a Queen
    I know other promotions are possible, but for the sake of testing I've just done queen

--- __tests__/Pawn.spec.js
+++ __tests__/Pawn.spec.js
@@ -2,6 +2,7 @@
 import Board from '../js/Board.js';
 import { SIZE, COLOUR } from '../js/constants.js';
 import Pawn from '../js/Pawn.js';
+import Queen from '../js/Queen.js';
 
 describe('Finding moves', () => {
     let tiles;
@@ -134,6 +135,37 @@ describe('Finding moves', () => {
 
         // then 
         expect(legalMoves.length).toBe(2);
+    });
+
+
+
+});
+
+
+describe('Should upgrade to a queen if at the edge of the board', () => {
+
+    let tiles;
+    beforeEach(() => {
+        const board = new Board();
+        tiles = board.createEmptyBoard();
+    });
+
+
+    test('Should upgrade white pawn to a queen', () => {
+        // given
+        const pawn = new Pawn(0, 1, COLOUR.WHITE);
+        pawn.move(0, 0, tiles);
+
+        expect(tiles[0][0] instanceof Queen).toBeTruthy();
+    });
+
+
+    test('Should upgrade black pawn to a queen', () => {
+        // given
+        const pawn = new Pawn(0, 6, COLOUR.BLACK);
+        pawn.move(0, 7, tiles);
+
+        expect(tiles[0][7] instanceof Queen).toBeTruthy();
     });
 
 
--- js/Pawn.js
+++ js/Pawn.js
@@ -1,5 +1,6 @@
 import { COLOUR } from './constants.js';
 import Piece from './Piece.js';
+import Queen from './Queen.js';
 export default class Pawn extends Piece {
     constructor(x, y, colour, sprite) {
         super(x, y, colour, sprite);
@@ -39,5 +40,18 @@ export default class Pawn extends Piece {
             }
         }
         return attacks;
+    }
+
+    move(x, y, tiles) {
+        super.move(x, y, tiles);
+        this.checkPromotion(y, tiles, x);
+    }
+
+    checkPromotion(y, tiles, x) {
+        if (this.colour == COLOUR.BLACK && y == 7) {
+            tiles[x][y] = new Queen(x, y, COLOUR.BLACK, '♛');
+        } else if (this.colour === COLOUR.WHITE && y === 0) {
+            tiles[x][y] = new Queen(x, y, COLOUR.WHITE, '♕');
+        }
     }
 }
